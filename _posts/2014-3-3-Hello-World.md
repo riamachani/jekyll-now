24bd10088dac09c83caf435e5857d42c78a10b9d
riamachani <riamachani123@gmail.com>
2017-04-01 15:06:03 +0530

    Update and rename _posts/2014-3-3-Hello-World.md to _posts2014-3-3-Helloworld.js

--- _posts/2014-3-3-Hello-World.md
+++
@@ -1,8 +0,0 @@
----
-layout: post
-title: Let's get cracking
----
-
-Here's a p5 example
-
-<iframe data-src='/p5/first' style='height: 400px'></iframe>
---
+++ _posts2014-3-3-Helloworld.js
@@ -0,0 +1,38 @@
+---
+layout: post
+title: my super pacman
+---
+
+Here's my amazing pacman
+
+<iframe data-src='/p5/first' style='height: 400px'></iframe>
+function setup() { 
+  createCanvas(400, 400);
+} 
+
+function draw() { 
+  background(220);
+}
+var y=0
+var x=150
+function draw() { 
+  background(0,0,0);
+	arc(x,150,50,50,PI/4,-PI/4,PIE)
+	if (y>20) {}
+	else {ellipse(170,150,5,5)
+				
+			 }
+	arc(x,150,50,50,PI/4,-PI/4,PIE)
+	if (y>40) {}
+	else {ellipse(190,150,5,5)
+			 
+			 }
+	arc(x,150,50,50,PI/4,-PI/4,PIE)
+	if (y>60) {}
+	else {ellipse(210,150,5,5)
+			 
+			 }
+	arc(x,150,50,50,PI/4,-PI/4,PIE)
+	y=y+1
+	x=x+1
+}
